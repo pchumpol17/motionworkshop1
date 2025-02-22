d0d9a57a4c3995638efaf1592d463f372e1cc1ba
Chumpol <pchumpol17@gmail.com>
2025-02-22 15:30:56 +0700

    motionworkshop1

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import {motion} from "framer-motion"
-import { div } from "framer-motion/client"
+import { motion } from "framer-motion"
 import Image from "next/image"
 
 interface ProductType {
